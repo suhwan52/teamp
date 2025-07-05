ec8edd3367ae053d53b57b483e36621bd2da8234
Jangeunji <dmswlquf@naver.com>
2025-07-05 20:35:03 +0900

    test

--- fixtures/urls.py
+++ fixtures/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import FixtureCreateView
+from .views import FixtureCreateView, FixtureUpdateView, FixtureDeleteView, FixtureListView
 
 urlpatterns = [
-    path('/api/admin/fixtures', FixtureCreateView.as_view(), name='fixture-create'),
+    path('api/admin/fixtures', FixtureCreateView.as_view(), name='fixture-create'),
+    path('api/admin/fixtures/{itemId}', FixtureUpdateView.as_view(), name='fixture-undate'),
+    path('api/admin/fixtures/{itemId}', FixtureDeleteView.as_view(), name='fixture-delete'),
+    path('api/admin/fixtures/{itemId}', FixtureListView.as_view(), name='fixture-list'),
 ]
