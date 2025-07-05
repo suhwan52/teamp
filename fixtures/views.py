aa927dec22507c0e9d12dce31386eb6408694688
Jangeunji <dmswlquf@naver.com>
2025-07-05 20:38:39 +0900

    commit

--- fixtures/views.py
+++ fixtures/views.py
@@ -5,9 +5,10 @@ from rest_framework.response import Response
 from rest_framework import status, permissions
 from .models import Fixture
 from .serializers import FixtureSerializer
+from django.http import Http404
 
 class FixtureCreateView(APIView):
-    permission_classes = [permissions.IsAuthenticated]  # Ïù∏Ï¶ù ÌïÑÏöî
+    # permission_classes = [permissions.IsAuthenticated]  #¿Œ¡ı
 
     def post(self, request):
         serializer = FixtureSerializer(data=request.data)
@@ -20,3 +21,62 @@ class FixtureCreateView(APIView):
                 "count": fixture.count
             }, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class FixtureUpdateView(APIView):    
+    def patch(self, request, itemId):
+        try:
+            fixture = Fixture.objects.get(id=itemId)
+        except Fixture.DoesNotExist:
+            return Response({"detail": "Not found."}, status=status.HTTP_404_NOT_FOUND)
+
+        serializer = FixtureSerializer(fixture, data=request.data, partial=True)
+        if serializer.is_valid():
+            fixture = serializer.save()
+            return Response({
+                "itemId": fixture.id,
+                "name": fixture.name,
+                "price": fixture.price,
+                "count": fixture.count
+            }, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+class FixtureDeleteView(APIView):
+    def delete(self, request, itemId):
+        try:
+            fixture = Fixture.objects.get(id=itemId)
+        except Fixture.DoesNotExist:
+            return Response({"detail": "Not found."}, status=status.HTTP_404_NOT_FOUND)
+        fixture.delete()
+        return Response({}, status=status.HTTP_400_BAD_REQUEST)
+    
+class FixtureListView(APIView):
+    def get(self, request):
+        page = int(request.GET.get('page',1))   #ƒı∏Æ ∆ƒ∂ÛπÃ≈Õ πﬁ±‚
+        size = int(request.GET.get('size',10))
+        #¿¸√º µ•¿Ã≈Õ ƒı∏Æº¬
+        queryset = Fixture.objects.all().order_by('id')
+        total_count = queryset.count()
+        total_page = (total_count + size - 1) // size
+
+        #∆‰¿Ã¬° ¿˚øÎ
+        start = (page - 1) * size
+        end = start + size
+        fixtures = queryset[start:end]
+
+        #µ•¿Ã≈Õ ¡˜∑ƒ»≠, itemId∑Œ ∫Ø»Ø
+        data = [
+            {
+                "itemId":obj.id,
+                "name": obj.name,
+                "price": obj.price,
+                "count": obj.count
+            }
+            for obj in fixtures
+        ]
+        return Response({
+            "page": page,
+            "size": size,
+            "totalPage": total_page,
+            "totalCount": total_count,
+            "data": data
+        }, status=status.HTTP_200_OK)
